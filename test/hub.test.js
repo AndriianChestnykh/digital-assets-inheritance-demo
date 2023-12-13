2f009fe03578641b1132d1c017562e7d496adbf3
ArtemHvozdov <artemhvozdov@gmail.com>
2023-12-13 17:44:34 +0200

    fix bugs of tests

--- test/hub.test.js
+++ test/hub.test.js
@@ -1,86 +1,108 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+    const { expect } = require("chai");
+    const { ethers } = require("hardhat");
+    const { utils } = require("ethers");
 
-describe("Hub Contract", function () {
-  let Hub;
-  let hubContract;
-  let owner;
-  let heir;
-  let oracle;
+    describe("Hub Contract", function () {
+    let Hub;
+    let hubContract;
+    let owner;
+    let heir;
+    let oracle;
 
-  beforeEach(async function () {
-    Hub = await ethers.getContractFactory("Hub");
-    [owner, heir, oracle] = await ethers.getSigners();
+    beforeEach(async function () {
+        Hub = await ethers.getContractFactory("Hub");
+        [owner, heir, oracle] = await ethers.getSigners();
 
-    hubContract = await Hub.deploy();
-    await hubContract.deployed();
-  });
+        hubContract = await Hub.deploy();
+        await hubContract.deployed();
+    });
 
-  it("should register public key for owner", async function () {
-    const publicKey = "0x0123456789ABCDEF"; // Exapmle data of public key by owner
+    // owner key registration testing
+    it("should register public key for owner", async function () {
+        const expectedPublicKey = "0x0123456789ABCDEF"; // Expected public key value by owner
 
-    await hubContract.connect(owner).registerPubKeyOwner(publicKey);
-    const registeredPublicKey = await hubContract.registeredKeys(owner.address);
+        await hubContract.connect(owner).registerPubKeyOwner(expectedPublicKey);
+        const registeredPublicKey = await hubContract.registeredKeys(owner.address);
 
-    expect(registeredPublicKey).to.equal(publicKey);
-  });
 
-  it("should register public key for heir", async function () {
-    const publicKey = "0xABCDEF0123456789"; // Exapmle data of public key by heir
+        const expectedKeyLower = expectedPublicKey.toLowerCase();
+        const actualKeyLower = registeredPublicKey.toLowerCase();
 
-    await hubContract.connect(owner).registerPubKeyHeir(heir.address, publicKey);
-    const registeredPublicKey = await hubContract.registeredKeys(heir.address);
+        expect(actualKeyLower).to.equal(expectedKeyLower);
+    });
 
-    const expectedKeyLower = expectedPublicKey.toLowerCase();
-    const actualKeyLower = registeredPublicKey.toLowerCase();
+    // heir key registration testing
+    it("should register public key for heir", async function () {
+        const expectedPublicKey = "0xABCDEF0123456789"; // Expected public key value by heir
 
-    expect(actualKeyLower).to.equal(expectedKeyLower);
-  });
+        await hubContract.connect(owner).registerPubKeyHeir(heir.address, expectedPublicKey);
+        const registeredPublicKey = await hubContract.registeredKeys(heir.address);
 
-  it("should register public key for oracle", async function () {
-    const publicKey = "0x9876543210FEDCBA"; // Exapmle data of public key by oracle
+        const expectedKeyLower = expectedPublicKey.toLowerCase();
+        const actualKeyLower = registeredPublicKey.toLowerCase();
 
-    await hubContract.connect(owner).registerPubKeyOracle(publicKey);
-    const registeredPublicKey = await hubContract.registeredKeys(owner.address);
+        expect(actualKeyLower).to.equal(expectedKeyLower);
+    });
 
-    expect(registeredPublicKey).to.equal(publicKey);
-  });
+    //oracle key registration testing
+    it("should register public key for oracle", async function () {
+        const expectedPublicKey = "0x9876543210FEDCBA"; // Expected public key value by oracle
+    
+        await hubContract.connect(owner).registerPubKeyOracle(expectedPublicKey);
+        const registeredPublicKey = await hubContract.registeredKeys(owner.address);
+    
+        expect(registeredPublicKey.toLowerCase()).to.equal(expectedPublicKey.toLowerCase());
+    });
 
-  it("should request message to publish", async function () {
-    await hubContract.connect(heir).requestToPublish();
-    const request = await hubContract.requests(heir.address);
+    it("should request message to publish", async function () {
+        await hubContract.connect(heir).requestToPublish();
+        const request = await hubContract.requests(heir.address);
 
-    expect(request.requested).to.equal(true);
-  });
+        expect(request.requested).to.equal(true);
+    });
 
-  it("should demand not to publish message", async function () {
-    await hubContract.connect(heir).demandNotToPublish(heir.address);
-    // Add assertions or checks as needed after this action
-  });
+    it("should demand not to publish message", async function () {
+        await hubContract.connect(heir).demandNotToPublish(heir.address);
+        // Add assertions or checks as needed after this action
+    });
 
-  it("should publish message", async function () {
-    const message = "Test message";
+    it("should publish message", async function () {
+        const message = "Test message";
 
-    await hubContract.connect(oracle).publish(heir.address, message);
-    // Add assertions or checks as needed after this action
-  });
+        await hubContract.connect(oracle).publish(heir.address, message);
+        // Add assertions or checks as needed after this action
+    });
 
-  it("should send EIM to Oracle", async function () {
-    const encryptedData = "0xencrypteddata"; // Example of encrypted data
+    it("should send EIM to Oracle", async function () {
+        const publicKey = "0x0123456789ABCDEF"; // Example public key
+        await hubContract.connect(owner).registerPubKeyOwner(publicKey); // Register owner's public key
+    
+        const encryptedData = utils.toUtf8Bytes("0xencrypteddata");
+        await hubContract.connect(owner).sendEIMtoOracle(encryptedData);
+    
+        // Add assertions or checks as needed after this action
+    });
 
-    await hubContract.connect(owner).sendEIMtoOracle(encryptedData);
-    // Add assertions or checks as needed after this action
-  });
+    //----------------------------
+    it("should request EIM discover by Heir", async function () {
+        const heirPublicKey = "0xABCDEF0123456789"; // Example public key by heir
+    
+        // Registering the public key Heir before the request
+        await hubContract.connect(heir).registerPubKeyOwner(heirPublicKey);
+    
+        // EIM Discovery Request
+        await hubContract.connect(heir).requestEIMDiscover();
+    });
 
-  it("should request EIM discover by Heir", async function () {
-    await hubContract.connect(heir).requestEIMDiscover();
-    // Add assertions or checks as needed after this action
-  });
+    it("should send EIM to Heir", async function () {
+        const encryptedData = ethers.utils.toUtf8Bytes("0xencrypteddata"); // Example of encrypted data
 
-  it("should send EIM to Heir", async function () {
-    const encryptedData = "0xencrypteddata"; // Example of encrypted data
+        // Register the public key for the Oracle
+        await hubContract.connect(oracle).registerPubKeyOracle("0x9876543210FEDCBA");
 
-    await hubContract.connect(oracle).sendEIMtoHeir(heir.address, encryptedData);
-    // Add assertions or checks as needed after this action
-  });
-});
+        // Execute the function after ensuring the Oracle's public key is registered
+        await hubContract.connect(oracle).sendEIMtoHeir(heir.address, encryptedData);
+
+        // Add assertions or checks as needed after this action
+    });
+    });
